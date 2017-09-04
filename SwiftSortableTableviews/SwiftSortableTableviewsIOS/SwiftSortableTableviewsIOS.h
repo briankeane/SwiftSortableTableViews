c6f4e68bbde96217b7e6298a2c09a18d6d13dd38
briankeane <lonesomewhistle@gmail.com>
2017-09-04 13:04:06 -0500

    setup scaffolding

---
+++ SwiftSortableTableviews/SwiftSortableTableviewsIOS/SwiftSortableTableviewsIOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSortableTableviewsIOS.h
+//  SwiftSortableTableviewsIOS
+//
+//  Created by Brian D Keane on 9/4/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSortableTableviewsIOS.
+FOUNDATION_EXPORT double SwiftSortableTableviewsIOSVersionNumber;
+
+//! Project version string for SwiftSortableTableviewsIOS.
+FOUNDATION_EXPORT const unsigned char SwiftSortableTableviewsIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSortableTableviewsIOS/PublicHeader.h>
+
+
---
+++ SwiftSortableTableviews/SwiftSortableTableviewsOSX/SwiftSortableTableviewsOSX.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSortableTableviewsOSX.h
+//  SwiftSortableTableviewsOSX
+//
+//  Created by Brian D Keane on 9/4/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftSortableTableviewsOSX.
+FOUNDATION_EXPORT double SwiftSortableTableviewsOSXVersionNumber;
+
+//! Project version string for SwiftSortableTableviewsOSX.
+FOUNDATION_EXPORT const unsigned char SwiftSortableTableviewsOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSortableTableviewsOSX/PublicHeader.h>
+
+
